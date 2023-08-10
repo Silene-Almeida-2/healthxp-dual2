23024e710963bbe6886ff3542f260b6d8dadda3e
Silene Pereira <silene2pereira@hotmail.com>
2023-08-09 23:23:35 -0300

    meu primeiro commit

---
+++ web/cypress/e2e/students.cy.js
@@ -0,0 +1,59 @@
+
+import students from '../fixtures/students.json'
+
+import studentPage from '../support/pages/StudentPage'
+
+describe('alunos', () => {
+
+    it('deve poder cadastrar um novo aluno', () => {
+        const student = students.create
+
+        cy.task('deleteStudent', student.email)
+        cy.adminLogin()
+
+        studentPage.goToRegister()
+        studentPage.submitForm(student)
+        studentPage.popup.haveText('Dados cadastrados com sucesso.')
+
+    })
+    it('não deve cadastrar com email duplicado', () => {
+        const student = students.duplicate
+
+
+        cy.task('resetStudent', student)
+
+        cy.adminLogin()
+
+        studentPage.goToRegister()
+        studentPage.submitForm(student)
+        studentPage.popup.haveText('O email informado já foi cadastrado!')
+    })
+
+    it('deve remover um aluno sem matricula', () => {
+        const student = students.remove
+
+        cy.task('resetStudent', student)
+
+        cy.adminLogin()
+
+        studentPage.search(student.name)
+        studentPage.remove(student.email)
+        studentPage.popup.confirm()
+        studentPage.popup.haveText('Exclusão realizada com sucesso.')
+    })
+
+    it('todos os campos são obrigatórios', () => {
+
+        const student = students.required
+
+        cy.adminLogin()
+        studentPage.goToRegister()
+        studentPage.submitForm(student)
+
+        studentPage.alertMessage('Nome completo', 'Nome é obrigatório')
+        studentPage.alertMessage('E-mail', 'O email é obrigatório')
+        studentPage.alertMessage('Idade', 'A idade é obrigatória')
+        studentPage.alertMessage('Peso (em kg)', 'O peso é obrigatório')
+        studentPage.alertMessage('Altura', 'A altura é obrigatória')
+   })
+})
